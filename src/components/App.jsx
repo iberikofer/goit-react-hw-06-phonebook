535ab79c4b1a07cd1a5abbeaed4342d1f9a23070
Yaroslav <sych521@gmail.com>
2023-07-08 09:34:48 +0300

    a little change =)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,7 @@ export const App = () => {
         gap: 50,
       }}
     >
-      <h1>Life cycle Phonebook</h1>
+      <h1>Redux Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
